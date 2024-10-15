2c166f11a57a1e925ec1e344114948e6baf5ecd4
retterrudi <paul.serdack+retterrudi@gmail.com>
2024-10-15 23:16:05 +0200

    Change text_type property to an enum class

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,22 +1,22 @@
 import unittest
 
-from src.textnode import TextNode
+from src.textnode import TextNode, TextType
 
 
 class TextTextNode(unittest.TestCase):
     def test_TextNode_eq(self):
-        node = TextNode('This is a text node', 'bold')
-        node2 = TextNode('This is a text node', 'bold')
+        node = TextNode('This is a text node', TextType.BOLD)
+        node2 = TextNode('This is a text node', TextType.BOLD)
         self.assertEqual(node, node2)
 
     def test_TextNode_eqWithGivenUrl_areEqual(self):
-        node = TextNode('This is a text node', 'bold', 'https://boot.dev')
-        node2 = TextNode('This is a text node', 'bold', 'https://boot.dev')
+        node = TextNode('This is a text node', TextType.BOLD, 'https://boot.dev')
+        node2 = TextNode('This is a text node', TextType.BOLD, 'https://boot.dev')
         self.assertEqual(node, node2)
 
     def test_TextNode_eqWithDifferingProperties_areNotEqual(self):
-        node = TextNode('This is a text node', 'bold')
-        node2 = TextNode('This is a text node', 'text')
+        node = TextNode('This is a text node', TextType.BOLD)
+        node2 = TextNode('This is a text node', TextType.ITALIC)
         self.assertNotEqual(node, node2)
 
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,19 @@
+from enum import Enum
+
+from src.htmlnode import LeafNode
+
+class TextType(Enum):
+    TEXT = 'text'
+    BOLD = 'bold'
+    ITALIC = 'italic'
+    CODE = 'code'
+    LINK = 'link'
+    IMAGE = 'image'
+
 class TextNode:
-    def __init__(self, text, text_type, url = None):
+    def __init__(self, text, text_type: TextType, url = None):
         self.text = text
-        self.text_type = text_type
+        self.text_type = text_type.value
         self.url = url
 
     def __eq__(self, other):
